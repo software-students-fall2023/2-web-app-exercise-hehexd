aab4b62e0aec382f622edb6e75e6283fa1bf5465
SpencerWPak <spencerpak9999@gmail.com>
2023-10-15 16:25:04 -0400

    combined functions

--- app/db.py
+++ app/db.py
@@ -5,21 +5,16 @@ database = client["database"]
 def get_all_events():
     return list(database.find({}))
     #return an array of all events in the database 
-    pass
-def search_event_type(type):
-    return list(database.find({"event_type": event_type}))
+def search_event_type(type, category):
+    if type is not None:
+        return list(database.find({"event_type": event_type}))
+
+    if category is not None:
+        return list(database.find({"category": category}))
     #seach for a particular event of the specified type
     #return the event as an object 
     # type is either "saving" or "spending"
     #return an array of matched events 
-    pass
-def search_event_category(category):
-    return list(database.find({"category": category}))
-    #seach for a particular event of the specified category 
-    #return the event as an object 
-    #return an array of matched events 
-    #type(category) == str 
-    pass
 def add_event(event):
     if database.find_one({"id": event["id"]}):
         print(f"Event with this ID {event['id']} already exists")
@@ -36,13 +31,10 @@ def add_event(event):
     #    'description': description,
     #    'category': category,
     #}
-    pass
 def modify_event(property, new_val):
     database.update_many({}, {"$set": {property: new_val}})
     #update all events with property=property to have new_val 
     #property:str new_val:str
-    pass
 def delete_event(property):
     events_collection.delete_one({"id": event_id})
     #delete an event with the specified property
-    pass
